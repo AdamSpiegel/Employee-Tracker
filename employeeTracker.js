55a8b57a6e678300b65565e5110f873be317e3e0
Your Name <you@example.com>
2021-06-14 08:35:42 -0400

    updates

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,102 @@
-const mysql = requiew('mysql');
+// Setting up npm dependencies
+const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
-const conne
+// Establising mymysql local host connection, establishing runTracker function
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Logan@0825!',
+    database: 'employee_trackerdb',
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    runTracker()
+});
+
+// Beginning the Employee Tracker by initializing inquirer to prompt the user with a set of questions
+const runTracker = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: "What would you like to do with your employee database?",
+        choices: [
+            'View All Employees',
+            'View All Employees By Department',
+            'View All Employees By Manager',
+            'Add An Employee',
+            'Remove An Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'Exit',
+        ],
+
+    })
+        // Switch statments to provide user with list of options of which to interact with the employeeTracker_db
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+
+                case 'View All Employees By Department':
+                    viewByDepartment();
+                    break;
+
+                case 'View All Employees By Manager':
+                    viewByManager();
+                    break;
+
+                case 'Add An Employee':
+                    addEmployee();
+                    break;
+
+                case 'Remove An Employee':
+                    removeEmployee();
+                    break;
+
+                case 'Update Employee Role':
+                    updateRole();
+                    break;
+
+                case 'Update Employee Manager':
+                    updateManager();
+                    break;
+
+                case 'Exit':
+                    exitTracker();
+                    break;
+
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+};
+// Creating viewEmployees function to first pull all employees located within the database
+function viewEmployees() {
+    var query = "SELECT * FROM employee";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log(res.length + " employees found");
+        console.table("All employees:", res);
+        runTracker();
+    });
+}
+// Creating viewByDepartment function to p
+function viewByDepartment() {
+    var query = "SELECT * FROM department";
+    if (err) throw err;
+    console.log(res.length + " employees found");
+    console.table("All departments:", res);
+    runTracker();
+}
+
+
+
+
+
+
--- seed.sql
+++ seed.sql
@@ -1,37 +1,28 @@
 USE employee_trackerDB;
 
 INSERT INTO department (name)
-VALUES (),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-();
+VALUES 
+("Management"), ("Sales"), ("Research and Development"), ("Production"), ("Human Resources"), ("Information Technology"), ("Accounting and Finance"), ("Marekting");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ();
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-();
+VALUES 
+("CEO", 195000, 1),
+("sales rep", 66000, 2),
+("senior scientist", 125000, 3),
+("manufacturing manager", 83000, 4),
+("hr recruiter", 53000, 5),
+("software engineer", 112000, 6),
+("treasurer", 145000, 7),
+("marketing analyst", 65000, 8);
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ();
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-(),
-();
+VALUES 
+("Monty", "Burns", 1, NULL),
+("Edna", "Krabappel", 2, 15),
+("John", "Frink", 3, NULL),
+("Homer", "Simspon", 4, 30),
+("Ned", "Flanders", 5, 15),
+("Seymour", "Skinner", 6, 45),
+("Maggie", "Simpson", 7, 45),
+("Moe", "Szyslak", 8, 60);
